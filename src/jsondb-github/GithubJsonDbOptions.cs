37e66f7b0c27d5c76df021d28997f59a98336941
8BitSensei <ckevlahan@gmail.com>
2024-03-24 22:18:54 +0000

    Add github jsondb

---
+++ src/jsondb-github/GithubJsonDbOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace JsonDb.Github
+{
+    public class GithubJsonDbOptions : JsonDbOptions
+    {
+        public string RepoOwner { get; set; }
+        public string RepoName { get; set; }
+        public string FilePath { get; set; }
+        public string PersonalAccessToken { get; set; }
+        public string Branch { get; set; } = "main";
+    }
+}
